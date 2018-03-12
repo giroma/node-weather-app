010803032cb3719c15fb9274255cb7b767ef0b1e
Argir Argirov <dj.giro@gmail.com>
2018-03-12 16:44:56 -0400

    encoding address from terminal into url api call

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv
 
+  let address = encodeURIComponent(argv.address)//encoded for url code
+
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${process.env.MAPS_API}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.MAPS_API}`,
   json: true
 }, (error, response, body) => {
   console.log(JSON.stringify(body.results[0].formatted_address, undefined, 2));
