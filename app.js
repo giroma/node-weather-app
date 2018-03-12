55ef202331646cfd72d0c034d0352fbac75a08d8
Argir Argirov <dj.giro@gmail.com>
2018-03-12 16:27:13 -0400

    spicific json data output

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ request({
   json: true
 }, (error, response, body) => {
   console.log(JSON.stringify(body.results[0].formatted_address, undefined, 2));
+  console.log('latitude:',JSON.stringify(body.results[0].geometry.location.lat, undefined, 2));
+  console.log('longitude:',JSON.stringify(body.results[0].geometry.location.lng, undefined, 2));
 })
