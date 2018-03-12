a88590a1b25475323ac80821e59e913e69dd9c8b
Argir Argirov <dj.giro@gmail.com>
2018-03-12 16:24:18 -0400

    using npm dotenv for my api key

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const request = require('request')
+
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${process.env.MAPS_API}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(JSON.stringify(body.results[0].formatted_address, undefined, 2));
 })
